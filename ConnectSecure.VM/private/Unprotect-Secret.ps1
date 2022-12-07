756917e2930f9e3056a962f284099f75d43dd23a
Simon Watson <sw@simon-watson.com>
2022-12-07 17:07:22 +0800

    add initial module files

---
+++ ConnectSecure.VM/private/Unprotect-Secret.ps1
@@ -0,0 +1,17 @@
+function Unprotect-Secret {
+    <#
+    .SYNOPSIS
+        Convert a [SecureString] to a [String]
+    .EXAMPLE
+        $plaintextsecret = Unprotect-Secret -Secret $clientsecret
+    #>
+    [CmdletBinding()]
+    [OutputType([System.String])]
+    param (
+        [Parameter(Mandatory = $true,
+            HelpMessage = 'The Secure String to convert to plain text')]
+        [SecureString]$Secret
+    )
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secret)
+    [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+}
